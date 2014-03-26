af6b5b29693a0224f5b50f3a86f450e5cd4392a4
blossom <blossom@blossom-IdeaPad-Y410P>
2014-03-26 10:33:16 -0400

    added card counting ability.

--- src/charlie/bot/server/BotBot.java
+++ src/charlie/bot/server/BotBot.java
@@ -26,6 +26,8 @@ public class BotBot implements IBot, Runnable{
     private List<Hid> hids;
     private int shoeSize = 0;
     private static int bustCount = 0; 
+    private static int runningCount = 0;
+    private static int trueCount = 0;
     private final long THINK_TIME = 2000;
     
     @Override
@@ -42,7 +44,10 @@ public class BotBot implements IBot, Runnable{
     public void sit(Seat seat) {
         hand = new Hand(new Hid(seat));
         hid = new Hid(hand.getHid());
-        hid.setAmt(5.0);
+        
+        //make a bet determined by the count
+        //math is done in getBetAmount()
+        hid.setAmt(5.0 * getBetAmount());
     }
 
     @Override
@@ -54,11 +59,17 @@ public class BotBot implements IBot, Runnable{
     @Override
     public void endGame(int shoeSize) {
         this.shoeSize = shoeSize;
+        
+        //updates the count.
+        updateTrueCount();
     }
 
     @Override
     public void deal(Hid hid, Card card, int[] values) {
-
+        //update the running count
+        //we do seem to get passed null cards from dealer?
+        if(card != null)
+            runningCount = runningCount + getHiLo(card);
         //we want the dealers card for checking the strategy...
         //this seems like a bit of a hack
         if(Seat.DEALER == hid.getSeat()){
@@ -112,7 +123,9 @@ public class BotBot implements IBot, Runnable{
 
     @Override
     public void shuffling() {
-        
+        //shuffle notification reset counts
+        trueCount = 0;
+        runningCount = 0;
     }
 
     @Override
@@ -249,5 +262,43 @@ public class BotBot implements IBot, Runnable{
     private Play getPlay(Hand hand, Card upCard){
         BasicStrategy bs = new BasicStrategy();
         return bs.advise(hand, upCard);
+    }
+    
+    /**
+     * Returns 1, -1 or 0 depending on the cards value
+     * @param card the card to "count"
+     * @return 1, -1, or 0 to "add" to count
+     */
+    private int getHiLo(Card card){
+        //if card is A,10,J,Q,K -1 to count
+        if(card.isAce() || card.isFace() || card.value() == 10)
+            return -1;
+        //if card is 2 - 6 add 1 to count
+        if(card.value() >= 2 && card.value() <= 6)
+            return 1;
+        //anything else we do not care about no change
+        return 0;
+    }
+    
+    /**
+     * Using the static variables determines how many
+     * chips to bet
+     * @return how many chips to bet
+     */
+    private int getBetAmount(){
+        //bet amount is max of 1 or 1 + true count
+        return Math.max(1, (1 + trueCount));
+    }
+    
+    /**
+     * Update the trueCount 
+     */
+    private void updateTrueCount(){
+        int decks = (this.shoeSize / 52);
+        //if less than 52 cards then remaining
+        //decks is less than one - just set to 1
+        if(decks <= 0)
+            decks = 1;
+        trueCount = (runningCount / decks);
     }
 }    
