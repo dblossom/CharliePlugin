a2ac988e5508b77efdf2d5967bcdcb92d33812b9
blossom <blossom@blossom-IdeaPad-Y410P>
2014-03-25 23:56:42 -0400

    removed testing methods, added comments, added split functionality, stream lined play method, cleared deal method... will use that for counting cards. Also found bug in new implementation of basic strategy and fix it... was looking for a non-existant location. Played no errors.

--- src/charlie/bot/server/BotBot.java
+++ src/charlie/bot/server/BotBot.java
@@ -9,12 +9,7 @@ import charlie.dealer.Seat;
 import charlie.plugin.IBot;
 import charlie.util.Play;
 import java.util.List;
-    
-//imports that are used to understand other methods
-//remove if not needed, or move to import lise if they are
-//just easier to delete 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,23 +18,15 @@ import java.util.logging.Logger;
  * @author D. Blossom, M. Ali, J. Muro
  */
 public class BotBot implements IBot, Runnable{
-
+    
     private Dealer dealer;
     private Hand hand;
     private Hid hid;
     private Card upCard;
-    static int deal_count = 0;
-    
-    //fields to try and understand these other methods
-    //and their purpose.
     private List<Hid> hids;
     private int shoeSize = 0;
-    private static int bustCount = 0;
-    private static int loseCount = 0;
-    private static int blackjackCount = 0;
-    private static int winCount = 0;
-    private static int pushCount = 0;
-    private static int charlieCount = 0;        
+    private static int bustCount = 0; 
+    private final long THINK_TIME = 2000;
     
     @Override
     public Hand getHand(){
@@ -62,43 +49,27 @@ public class BotBot implements IBot, Runnable{
     public void startGame(List<Hid> hids, int shoeSize) {
         this.hids = new ArrayList<>(hids);
         this.shoeSize = shoeSize;
-        System.out.println("********** startGame(List<Hid>, int) **********");
-        for(int i = 0; i < hids.size(); i++){
-            System.out.println("Item @ " + i + ": " + hids.get(i));
-        }
-        System.out.println("Shoe Size @ start of game: " + this.shoeSize);
     }
 
     @Override
     public void endGame(int shoeSize) {
         this.shoeSize = shoeSize;
-        System.out.println("Shoe size @ end of game: " + this.shoeSize);
-        System.out.println("WINS: " + winCount);
-        System.out.println("LOSES: " + loseCount);
-        System.out.println("BUSTS: " + bustCount);
-        System.out.println("BLACKJACKS: " + blackjackCount);
-        System.out.println("CHARLIES: " + charlieCount);
-        System.out.println("PUSHES: " + pushCount);
     }
 
     @Override
     public void deal(Hid hid, Card card, int[] values) {
-        System.out.println("********** START **********");
-        System.out.println("deal(hid, card, int[]) call #: " + (++deal_count));
-        if(card != null)
-            System.out.println("The card is: " + card.toString());
-        System.out.println("The seat for this call: " + hid.getSeat());
-        System.out.println("**********  END  **********");
 
         //we want the dealers card for checking the strategy...
         //this seems like a bit of a hack
         if(Seat.DEALER == hid.getSeat()){
-            Hand h = new Hand(hid);
             if(card != null)
                 upCard = new Card(card);
         }
+        
+        //this is the bots hand not sure what to do with
+        //this stuff yet. Spawn a thread to maybe hit again?
         if(hid.getSeat() == this.hid.getSeat()){
-            //nothing
+            
         }
     }
 
@@ -110,43 +81,33 @@ public class BotBot implements IBot, Runnable{
     @Override
     public void bust(Hid hid) {
         if(this.hid == hid){
-            System.out.println("Busts: " + (++bustCount));
+            ++bustCount;
         }
     }
     
     @Override
     public void win(Hid hid) {
-        if(this.hid == hid){
-            System.out.println("Wins: " + (++winCount));
-        }
+
     }
 
     @Override
     public void blackjack(Hid hid) {
-        if(this.hid == hid){
-            System.out.println("Blackjacks: " + (++blackjackCount));
-        }
+
     }
 
     @Override
     public void charlie(Hid hid) {
-        if(this.hid == hid){
-            System.out.println("Charlies: " + (++charlieCount));
-        }
+
     }
 
     @Override
     public void lose(Hid hid) {
-        if(this.hid == hid){
-            System.out.println("Loses: " + (++loseCount));
-        }
+
     }
 
     @Override
     public void push(Hid hid) {
-        if(this.hid == hid){
-            System.out.println("Pushes: " + (++pushCount));
-        }
+
     }
 
     @Override
@@ -165,40 +126,69 @@ public class BotBot implements IBot, Runnable{
     public void run() {
                 
         synchronized(this){
-                        
+            
+            //look like we are thinking
+            //do we want to make this random
+            //also do we want the entire synch
+            //block in the try statement
+            try {
+                wait(THINK_TIME);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BotBot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            //lets get a suggestion off strategy card
             Play suggestion = getPlay(hand, upCard);
             
+            //split is not implemented so we need to
+            //make an alternative move
             if(suggestion == Play.SPLIT){
-                System.out.println("SPLIT");
                 suggestion = splitAltPlay(hand, upCard);
             }
             
+            //we might want to hit more than just once
+            //also if play is double and not first move
+            //we cannot do that, so since double is taking
+            //a hit, just return hit.
             while(suggestion == Play.HIT || 
                  (suggestion == Play.DOUBLE_DOWN && hand.size() != 2)){
                 
-                System.out.println("HIT / DOUBLE LOOP");
                 dealer.hit(this, this.hid);
-                suggestion = getPlay(hand, upCard);
                 
                 if(hand.getValue() == 21 || hand.isBroke())
-                    return;
-
+                    break;
+                
+                try {
+                    Thread.sleep(THINK_TIME);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(BotBot.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                //get next suggestion
+                suggestion = getPlay(hand, upCard);
             }
             
+            //double, add to bet and return
             if(suggestion == Play.DOUBLE_DOWN && hand.size() == 2){
-                System.out.println("DOUBLE");
                 hid.dubble();
                 dealer.doubleDown(this, this.hid);
                 return;
             }
             
+            //just going to stay
             if(suggestion == Play.STAY){
-                System.out.println("STAY");
                 dealer.stay(this, this.hid);
             }
         }
     }
     
+    /**
+     * Method returns an alternative to split since it is not implemented
+     * basically uses value and forgets it is a pair
+     * @param hand the hand to revalue
+     * @param upCard dealers card
+     * @return the new play
+     */
     private Play splitAltPlay(Hand hand, Card upCard){
         
         //how did we even get in here?
@@ -250,6 +240,12 @@ public class BotBot implements IBot, Runnable{
         
     }
     
+    /**
+     * Returns a basic strategy play
+     * @param hand hand to evaluate
+     * @param upCard dealers up card
+     * @return suggested play
+     */
     private Play getPlay(Hand hand, Card upCard){
         BasicStrategy bs = new BasicStrategy();
         return bs.advise(hand, upCard);
